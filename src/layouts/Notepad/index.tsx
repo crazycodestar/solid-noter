ba689472cd859e5e3f2337b30f9164bb1d3dc4d8
olalekan <olamilekanadekanmbi@gmail.com>
2023-01-02 13:13:09 +0100

    create autosave for notepad

--- src/layouts/Notepad/index.tsx
+++ src/layouts/Notepad/index.tsx
@@ -1,68 +1,79 @@
-import { createTiptapEditor } from "solid-tiptap";
+import { createTiptapEditor, useEditorHTML } from "solid-tiptap";
 import StarterKit from "@tiptap/starter-kit";
 import BubbleMenu from "@tiptap/extension-bubble-menu";
-import { createSignal, JSX, Show } from "solid-js";
+import { createEffect, createMemo, createSignal, JSX, Show, untrack, on, Accessor } from "solid-js";
 import { Toolbar } from "solid-headless";
-import { CONTENT } from "./components/content";
 import { ToolbarContents } from "./components/ToolbarContents";
-import { NoteType, ExtractType, Id } from "../../pages/Notes";
+import { NoteType } from "../../pages/Notes";
 
 interface INotepadProps {
-	updateNote: (
-		id: Id,
-		newVal: { [T in keyof NoteType]: ExtractType<NoteType, T> }
-	) => void;
-	note: NoteType | undefined;
+  onUpdateTitle: (title: string) => void;
+  onSaveNote: (note: string) => void;
+  note: NoteType;
+  triggerSignal: Accessor<number>;
 }
 
 export function Notepad(props: INotepadProps): JSX.Element {
-	const [container, setContainer] = createSignal<HTMLDivElement>();
-	const [menu, setMenu] = createSignal<HTMLDivElement>();
+  const [container, setContainer] = createSignal<HTMLDivElement>();
+  const [menu, setMenu] = createSignal<HTMLDivElement>();
 
-	const editor = createTiptapEditor(() => ({
-		element: container()!,
-		extensions: [
-			StarterKit,
-			BubbleMenu.configure({
-				element: menu()!,
-			}),
-		],
-		editorProps: {
-			attributes: {
-				class: "focus:outline-none w-full content mb-16",
-			},
-		},
-		content: props.note?.note.content || "",
-	}));
+  const content = () => props.note?.note.content;
+  const editor = createMemo(on(props.triggerSignal, () => {
+    return createTiptapEditor(() => {
+      console.log('re-rendering editor')
+      const contentData = untrack(content);
+      return ({
+        element: container()!,
+        extensions: [
+          StarterKit,
+          BubbleMenu.configure({
+            element: menu()!,
+          }),
+        ],
+        editorProps: {
+          attributes: {
+            class: "focus:outline-none w-full content mb-16",
+          },
+        },
+        content: contentData || "",
+      })
+    })
+  }))
 
-	return (
-		<div class="md:mt-6 md:rounded-md md:mb-6 md:mr-6 w-full bg-white flex flex-col">
-			<Toolbar
-				ref={setMenu}
-				class="dynamic-shadow bg-gradient-to-bl from-indigo-500 to-blue-600 text-white rounded-lg"
-				horizontal
-			>
-				<Show when={editor()} keyed>
-					{(instance) => <ToolbarContents editor={instance} />}
-				</Show>
-			</Toolbar>
-			<div class="text-lg font-semibold capitalize pb-2 pt-4 px-4 mb-3 border-b-2 flex space-x-2 items-center">
-				<h1>{props.note ? props.note.filename : "no File selected"}</h1>
-			</div>
-			<div class="pt-16 overflow-y-auto h-full scrollbar-thin scrollbar-thumb-slate-300 scrollbar-track-transparent w-full max-h-max px-6 flex flex-col">
-				<Show when={Boolean(props.note)}>
-					<div class="flex flex-col items-center h-full">
-						<textarea
-							cols={1}
-							rows={1}
-							value={props.note?.note.title}
-							placeholder="Title"
-							class="resize-none scrollbar-none focus:outline-none max-w-2xl w-full text-3xl mb-4 tracking-wide"
-						/>
-						<div class="max-w-2xl w-full h-full" ref={setContainer} />
-					</div>
-				</Show>
-			</div>
-		</div>
-	);
+  const html = useEditorHTML(() => editor()());
+  createEffect(() => {
+    const result = html()
+    if (result) props.onSaveNote(result);
+  })
+
+  return (
+    <div class="md:mt-6 md:rounded-md md:mb-6 md:mr-6 w-full bg-white flex flex-col">
+      <Toolbar
+        ref={setMenu}
+        class="dynamic-shadow bg-gradient-to-bl from-indigo-500 to-blue-600 text-white rounded-lg"
+        horizontal
+      >
+        <Show when={editor()()} keyed>
+          {(instance) => <ToolbarContents editor={instance} />}
+        </Show>
+      </Toolbar>
+      <div class="text-lg font-semibold capitalize pb-2 pt-4 px-4 mb-3 border-b-2 flex space-x-2 items-center">
+        <h1>{props.note.filename}</h1>
+      </div>
+      <div class="pt-16 overflow-y-auto h-full scrollbar-thin scrollbar-thumb-slate-300 scrollbar-track-transparent w-full max-h-max px-6 flex flex-col">
+        <div class="flex flex-col items-center h-full">
+          {/*TODO: resize not working for title text-area*/}
+          <textarea
+            cols={1}
+            rows={1}
+            value={props.note.note.title}
+            onInput={e => props.onUpdateTitle(e.currentTarget.value)}
+            placeholder="Title"
+            class="resize-none scrollbar-none focus:outline-none max-w-2xl w-full text-3xl mb-4 tracking-wide"
+          />
+          <div class="max-w-2xl w-full h-full" ref={setContainer} />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -1,4 +1,4 @@
-import { createSignal } from "solid-js";
+import { createMemo, createSignal, For, Show, untrack } from "solid-js";
 import { FileTree } from "../layouts/FileTree";
 import { Notepad } from "../layouts/Notepad";
 import produce from "immer";
@@ -6,120 +6,146 @@ import produce from "immer";
 const PLACEHOLDER = "<P> Start Writing Here </p>";
 
 export type NoteType = {
-	filename: string;
-	note: {
-		title: string;
-		content: string;
-	};
-	id: number;
-	selected: boolean;
+  filename: string;
+  note: {
+    title: string;
+    content: string;
+  };
+  id: number;
+  selected: boolean;
 };
 
 export type ExtractType<T, K extends keyof T> = T[K];
 export type Id = ExtractType<NoteType, "id">;
 
 const NOTES = [
-	{
-		id: 0,
-		filename: "file 1",
-		note: {
-			title: "",
-			content: PLACEHOLDER,
-		},
-	},
+  {
+    id: 0,
+    filename: "file 1",
+    note: {
+      title: "",
+      content: PLACEHOLDER,
+    },
+  },
 ];
 
 const Notes = () => {
-	const initNotes: NoteType[] = NOTES.map((note) => ({
-		...note,
-		selected: false,
-	}));
+  const initNotes: NoteType[] = NOTES.map((note) => ({
+    ...note,
+    selected: false,
+  }));
 
-	// TODO: convert from createSignal to createStore or use Zustand anyone
-	const [notes, setNotes] = createSignal<NoteType[]>(initNotes);
+  // TODO: convert from createSignal to createStore or use Zustand anyone
+  const [notes, setNotes] = createSignal<NoteType[]>(initNotes);
 
-	// const selectNote = (id: Id) =>
-	// 	setNotes((init) => {
-	// 		return produce<NoteType[]>((init, draft) => {
-	// 			const note = draft.find((note) => note.id === id);
-	// 			if (!note) return;
-	// 			note.selected = true;
-	// 		})();
-	// 	});
+  const handleSelectNote = (id: Id) => {
+    const noteId = notes().findIndex((note) => note.id === id);
+    if (noteId === -1) return;
 
-	const handleSelectNote = (id: Id) => {
-		const noteId = notes().findIndex((note) => note.id === id);
-		if (noteId === -1) return;
-		return setNotes((init) => {
-			const newState = init.map((notes) => ({ ...notes, selected: false }));
+    const newNotes = notes().map(note => ({ ...note, selected: false }));
+    newNotes[noteId].selected = true;
 
-			const newNote = { ...newState[noteId], selected: true };
-			newState[noteId] = newNote;
+    setNotes(newNotes);
 
-			return newState;
-		});
-	};
+  };
 
-	const handleCreateNote = () => {
-		setNotes((init) => [
-			...init.map((note) => ({ ...note, selected: false })),
-			{
-				id: init.length,
-				colorTag: "#fff000",
-				note: {
-					title: "",
+  const handleCreateNote = () => {
+    setNotes((init) => [
+      ...init.map((note) => ({ ...note, selected: false })),
+      {
+        id: init.length,
+        note: {
+          title: "",
 
-					content: PLACEHOLDER,
-				},
-				filename: "New File",
-				selected: true,
-			},
-		]);
-	};
+          content: PLACEHOLDER,
+        },
+        filename: "New File",
+        selected: true,
+      },
+    ]);
+  };
 
-	const handleDeleteNote = (id: Id) => {
-		setNotes((init) => init.filter((note) => note.id !== id));
-	};
+  const handleDeleteNote = (id: Id) => {
+    setNotes((init) => init.filter((note) => note.id !== id));
+  };
 
-	const updateNote = (
-		id: Id,
-		newVal: { [T in keyof NoteType]: ExtractType<NoteType, T> }
-	) => {
-		setNotes((init) => {
-			return produce<NoteType[]>((init, draft) => {
-				const note = draft.find((note) => note.id === id);
-				if (!note) return;
-				Object.assign(note, newVal);
-			})();
-		});
-	};
+  const updateNote = (
+    id: Id,
+    newVal: { [T in keyof NoteType]: ExtractType<NoteType, T> }
+  ) => {
+    setNotes((init) => {
+      return produce<NoteType[]>((init, draft) => {
+        const note = draft.find((note) => note.id === id);
+        if (!note) return;
+        Object.assign(note, newVal);
+      })();
+    });
+  };
 
-	const handleSetNoteName = (id: Id, name: string) => {
-		const index = notes().findIndex((note) => note.id === id);
-		if (index === -1) return;
+  const handleSetNoteName = (id: Id, name: string) => {
+    const index = notes().findIndex((note) => note.id === id);
+    if (index === -1) return;
 
-		const nextState = produce<NoteType[]>((draft = notes()) => {
-			draft[index].filename = name;
-		});
-		// TODO: make check if the state is properly immutable and not just duplicating the entire thing
-		setNotes((state) => nextState(state));
-	};
+    const nextState = produce<NoteType[]>((draft = notes()) => {
+      draft[index].filename = name;
+    });
+    // TODO: make check if the state is properly immutable and not just duplicating the entire thing
+    setNotes((state) => nextState(state));
+  };
 
-	const note = () => notes().find((note) => note.selected === true);
+  const handleSaveNote = (id: Id, note: string) => {
+    const index = notes().findIndex((note) => note.id === id);
+    if (index === -1) return;
 
-	return (
-		<div class="font-Source_Sans_Pro h-screen md:flex bg-slate-100">
-			{/* <pre>{JSON.stringify(notes(), null, 2)}</pre> */}
-			<FileTree
-				onSelectNote={handleSelectNote}
-				onCreateNote={handleCreateNote}
-				onDeleteNote={handleDeleteNote}
-				onSetNoteName={handleSetNoteName}
-				notes={notes()}
-			/>
-			<Notepad note={note()} updateNote={updateNote} />
-		</div>
-	);
+    const nextState = produce<NoteType[]>((draft = notes()) => {
+      draft[index].note.content = note;
+    });
+    // TODO: make check if the state is properly immutable and not just duplicating the entire thing
+    setNotes((state) => nextState(state));
+  };
+
+  const handleUpdateTitle = (id: Id, title: string) => {
+    const index = notes().findIndex((note) => note.id === id);
+    if (index === -1) return;
+
+    const nextState = produce<NoteType[]>((draft = notes()) => {
+      draft[index].note.title = title;
+    });
+    // TODO: make check if the state is properly immutable and not just duplicating the entire thing
+    setNotes((state) => nextState(state));
+  };
+
+  const note = () => notes().find((note) => note.selected === true);
+  const triggerSignal = () => createMemo(() => {
+    return notes().findIndex(note => note.selected);
+  })
+
+  return (
+    <div class="font-Source_Sans_Pro h-screen md:flex bg-slate-100">
+      <FileTree
+        onSelectNote={handleSelectNote}
+        onCreateNote={handleCreateNote}
+        onDeleteNote={handleDeleteNote}
+        onSetNoteName={handleSetNoteName}
+        notes={notes()}
+      />
+      {/* TODO: create a git:branch where the notes can be stacked on each other */}
+      {/* <For each={notes()}> */}
+      {/*   {(note) => { */}
+      {/*     return ( */}
+      {/*       <> */}
+      {/*         {/1* <Show when={note.selected}> *1/} */}
+      {/*         <Notepad note={note} onSaveNote={(content) => handleSaveNote(note.id, content)} onUpdateTitle={(title) => handleUpdateTitle(note.id, title)} /> */}
+      {/*         {/1* </Show> *1/} */}
+      {/*       </> */}
+      {/*     ) */}
+      {/*   }} */}
+      {/* </For> */}
+      <Show when={Boolean(note())}>
+        <Notepad triggerSignal={triggerSignal()} note={note()} onSaveNote={(content) => handleSaveNote(note()!.id, content)} onUpdateTitle={(title) => handleUpdateTitle(note()!.id, title)} />
+      </Show>
+    </div>
+  );
 };
 
 export default Notes;
